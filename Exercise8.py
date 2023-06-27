b65ff9f5dca7f033391e3d919fc1973306b6e018
Linjingyu8888 <linjingyu94@126.com>
2023-06-27 21:53:29 +0200

    Exercise8

--- Exercise8.py
+++ Exercise8.py
@@ -93,13 +93,14 @@ def greet_users(names):
 usernames = ['hannah', 'ty', 'margot']
 greet_users(usernames)
 
+'''------------------------------------------------------'''
 # 传递列表并修改列表：将列表传递给函数后，函数就可对其进行修改。在函数中对这个列表所做的任何修改都是永久性的。
 ## 首先创建一个列表，其中包含一些要打印的设计
 unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
 completed_models = []
 ## 模拟打印每个设计，直到没有未打印的设计为止
 ## 打印每个设计后，都将其移到列表completed_models中
-while unprinted_designs:
+while len(unprinted_designs) != 0:
     current_design =  unprinted_designs.pop()
     print(f"Printing model: {current_design}")
     completed_models.append(current_design)
@@ -107,3 +108,52 @@ while unprinted_designs:
 print("\nThe following models have been printed:")
 for completed_model in completed_models:
     print(completed_model)
+
+'''-----------------------------------------------------'''
+# 为重新组织这些代码，可编写两个函数，每个都做意见具体的工作。大部分代码与原来相同，只是效率更高。
+# 第一个负责处理打印设计的工作，第二个概述打印了哪些设计。
+
+def print_models(unprinted_designs, completed_models):
+    '''模拟打印每个设计，直到没有未打印设计为止。打印每个设计后，都将其移到列表completed_models中'''
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model:{current_design}")
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    '''显示打印好的所有模型'''
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
+
+# 禁止函数修改列表。由于前一个函数将所有的设计都移出了unprinted_designs，这个列表变成了空的，原来的列表没有了。
+# 为了解决这个问题，可向函数传递列表的副本而非原件。
+print_models(unprinted_designs[:], completed_models)
+#unprinted_desgins[:]即为列表的副本。
+'''-----------------------------------------------'''
+# 将函数存储在模块
+import pizza
+pizza.make_pizza(16, 'tomato', 'mushroom', 'onion')
+
+'''----------------------------------------------'''
+# 导入特定的函数
+from pizza import make_pizza
+make_pizza(16, 'tomato', 'mushroom', 'onion')
+
+# 使用as给函数指定别名
+from pizza import make_pizza as mp
+mp(16, 'tomato', 'mushroom', 'onion')
+
+# 使用as给模块指定别名
+import pizza as p
+p.make_pizza(16, 'tomato', 'mushroom', 'onion')
+
+# 导入模块中的所有函数
+from pizza import*
+make_pizza(16, 'tomato', 'mushroom', 'onion')
