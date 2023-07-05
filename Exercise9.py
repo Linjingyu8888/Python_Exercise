4099ef1fd19051f64a96c797dfadb4b70f565891
Linjingyu8888 <linjingyu94@126.com>
2023-07-05 20:30:18 +0200

    Exercise9

--- Exercise9.py
+++ Exercise9.py
@@ -39,7 +39,7 @@ class Car:
         """打印一条指出汽车里程的消息"""#在创建实列时，有些属性无须通过形参来定义，可在方法__init__()中为其指定默认值。
         print(f"This car has {self.odometer_reading} miles on it.")
 
-my_new_car = Car('audi', 'a4', '2019')
+my_new_car = Car('audi1', 'a4', '2019')
 print(my_new_car.get_descriptive_name())
 # my_new_car.odometer_reading = 23 #要修改属性的值，最简单的方式是通过实例直接访问它。
 my_new_car.read_odometer()
@@ -68,7 +68,119 @@ class Car:
     #添加方法update_odometer()，这个方法接受一个里程值，并将其赋给self.odometer_reading.
         self.odometer_reading = mileage 
 
-my_new_car = Car('audi', 'a4', '2019')
+my_new_car = Car('audi2', 'a4', '2019')
 print(my_new_car.get_descriptive_name())
 my_new_car.update_odometer(23) # 调用update_odometer并向它提供了实参23.
 my_new_car.read_odometer() 
+
+"""-----------------------------------------------------------------------------"""
+#禁止任何人将里程表读数往回调
+class Car:
+    """一次模拟汽车的简单尝试"""
+    def __init__(self, make, model, year):
+        """初始化描述汽车的属性。"""
+        self.make = make 
+        self.model = model
+        self.year = year
+        self.odometer_reading = 50 #在创建实列时，有些属性无须通过形参来定义，可在方法__init__()中为其指定默认值。
+
+    def get_descriptive_name(self):
+        """返回整洁的描述性信息"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """打印一条指出汽车里程的消息"""#在创建实列时，有些属性无须通过形参来定义，可在方法__init__()中为其指定默认值。
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage): 
+    #将里程表读数设置为指定的值，禁止将里程表读书往回调。
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+my_new_car = Car('audi3', 'a4', '2019')
+print(my_new_car.get_descriptive_name())
+my_new_car.update_odometer(2) # 调用update_odometer并向它提供了实参2.
+my_new_car.read_odometer() 
+
+"""-----------------------------------------------------------------------------"""
+#禁止任何人将里程表读数往回调，且通过方法对属性的值进行递增
+class Car:
+    """一次模拟汽车的简单尝试"""
+    def __init__(self, make, model, year):
+        """初始化描述汽车的属性。"""
+        self.make = make 
+        self.model = model
+        self.year = year
+        self.odometer_reading = 50 #在创建实列时，有些属性无须通过形参来定义，可在方法__init__()中为其指定默认值。
+
+    def get_descriptive_name(self):
+        """返回整洁的描述性信息"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """打印一条指出汽车里程的消息"""#在创建实列时，有些属性无须通过形参来定义，可在方法__init__()中为其指定默认值。
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage): 
+    #将里程表读数设置为指定的值，禁止将里程表读书往回调。
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        """将里程表读数增加指定的量"""
+        self.odometer_reading += miles #syntactic sugar: 相当于self.odometer_reading = self.odometer_reading+miles
+
+
+my_new_car = Car('subaru', 'a5', '2015')
+print(my_new_car.get_descriptive_name())
+
+my_new_car.update_odometer(666) # 调用update_odometer并向它提供了实参
+my_new_car.read_odometer() 
+
+my_new_car.increment_odometer(100)
+my_new_car.read_odometer()
+
+"""-----------------------------------------------------------------------------"""
+class Car:
+    """一次模拟汽车的简单尝试"""
+    def __init__(self, make, model, year):
+        """初始化描述汽车的属性。"""
+        self.make = make 
+        self.model = model
+        self.year = year
+        self.odometer_reading = 50 #在创建实列时，有些属性无须通过形参来定义，可在方法__init__()中为其指定默认值。
+
+    def get_descriptive_name(self):
+        """返回整洁的描述性信息"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """打印一条指出汽车里程的消息"""#在创建实列时，有些属性无须通过形参来定义，可在方法__init__()中为其指定默认值。
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage): 
+    #将里程表读数设置为指定的值，禁止将里程表读书往回调。
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        """将里程表读数增加指定的量"""
+        self.odometer_reading += miles
+
+class ElectricCar (Car):
+    """电动汽车的独特之处。(创建子类时，父类必须包含在当前文件中，且位于子类前面。定义子类时，必须在圆括号内指定父类的名称。)"""
+    def __init__(self, make, model, year):
+        """初始化父类的属性"""
+        super().__init__(make, model, year)
+        #super()是一个特殊函数，使能够调用父类的方法。这行代码让Python调用Car类的方法__init__()
+        #让ElectricCar实例包含这个方法中定义的所有属性。父类也称为超类(superclass)。
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
